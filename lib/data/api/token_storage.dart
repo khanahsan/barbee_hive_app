3a87c749fceb014cf24f5a0222b8e5779e4d2208
syedumair043 <syedumairbadar39@gmail.com>
2025-05-09 19:52:00 +0500

    changes in login model

--- lib/data/api/token_storage.dart
+++ lib/data/api/token_storage.dart
@@ -3,6 +3,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class TokenStorage {
   static const _tokenKey = 'auth_token';
+  static const _userIdKey = 'user_id';
+  static const _roleKey = 'user_role';
 
   static Future<void> saveToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
@@ -14,8 +16,34 @@ class TokenStorage {
     return prefs.getString(_tokenKey);
   }
 
+  // Save user ID
+  static Future<void> saveUserId(int userId) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_userIdKey, userId);
+  }
+
+  // Get user ID
+  static Future<int?> getUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(_userIdKey);
+  }
+
+  // Save role
+  static Future<void> saveRole(int role) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_roleKey, role);
+  }
+
+  // Get role
+  static Future<int?> getRole() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(_roleKey);
+  }
+
   static Future<void> clearToken() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove(_tokenKey);
+    await prefs.remove(_userIdKey);
+    await prefs.remove(_roleKey);
   }
 }
